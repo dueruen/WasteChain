d19a9098240582cf63bba10edba6e8109db03d8a
Mads Due Kristensen <32197042+dueruen@users.noreply.github.com>
2019-11-16 16:55:54 +0900

    Update resolver.go

--- service/api_gateway/graphql/resolver.go
+++ service/api_gateway/graphql/resolver.go
@@ -1,91 +1,10 @@
 package graphql
 
-// import (
-// 	"context"
-// )
-
-// // type Resolver struct {
-// // 	accountClient pb.AccountServiceClient
-// // }
-
-// // func (r *Resolver) Mutation() MutationResolver {
-// // 	return &mutationResolver{r}
-// // }
-// // func (r *Resolver) Query() QueryResolver {
-// // 	return &queryResolver{r}
-// // }
-
-// // type mutationResolver struct{ *Resolver }
-
-// // func (r *mutationResolver) CreateCompany(ctx context.Context, company *CreateCompany) (*Company, error) {
-// // 	res, err := r.accountClient.CreateCompany(ctx, &pb.CreateCompanyRequest{company})
-// // 	if err != nil {
-// // 		return nil, err
-// // 	}
-// // 	return nil, res.Company
-// // }
-
-// // func (r *mutationResolver) CreateEmployee(ctx context.Context, employee CreateEmployee) (*Employee, error) {
-// // 	panic("not implemented")
-// // }
-
-// // type queryResolver struct{ *Resolver }
-
-// // func (r *queryResolver) ListAllCompanies(ctx context.Context) ([]*Company, error) {
-// // 	panic("not implemented")
-// // }
-// // func (r *queryResolver) GetCompany(ctx context.Context, companyID string) (*Company, error) {
-// // 	panic("not implemented")
-// // }
-// // func (r *queryResolver) ListAllEmployeesInCompany(ctx context.Context, companyID string) ([]*Employee, error) {
-// // 	panic("not implemented")
-// // }
-// // func (r *queryResolver) GetEmployee(ctx context.Context, employeeID string) (*Employee, error) {
-// // 	panic("not implemented")
-// // }
-
-// type Resolver struct{}
-
-// func (r *Resolver) Mutation() MutationResolver {
-// 	return &mutationResolver{r}
-// }
-// func (r *Resolver) Query() QueryResolver {
-// 	return &queryResolver{r}
-// }
-
-// type mutationResolver struct{ *Resolver }
-
-// func (r *mutationResolver) CreateCompany(ctx context.Context, company *CreateCompany) (*Company, error) {
-// 	panic("not implemented")
-// }
-// func (r *mutationResolver) CreateEmployee(ctx context.Context, employee CreateEmployee) (*Employee, error) {
-// 	panic("not implemented")
-// }
-
-// type queryResolver struct{ *Resolver }
-
-// func (r *queryResolver) ListAllCompanies(ctx context.Context) ([]*Company, error) {
-// 	panic("not implemented")
-// }
-// func (r *queryResolver) GetCompany(ctx context.Context, companyID string) (*Company, error) {
-// 	panic("not implemented")
-// }
-// func (r *queryResolver) ListAllEmployeesInCompany(ctx context.Context, companyID string) ([]*Employee, error) {
-// 	panic("not implemented")
-// }
-// func (r *queryResolver) GetEmployee(ctx context.Context, employeeID string) (*Employee, error) {
-// 	panic("not implemented")
-// }
-
-//package tmp
-
 import (
 	"context"
 
 	pb "github.com/dueruen/WasteChain/service/api_gateway/gen/proto"
 )
-
-// THIS CODE IS A STARTING POINT ONLY. IT WILL NOT BE UPDATED WITH SCHEMA CHANGES.
 
 type Resolver struct {
 	AccountClient pb.AccountServiceClient
