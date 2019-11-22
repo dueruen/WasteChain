30185e9e4a830613147c822a8733985567c660b0
Victor Gram Thomsen <victor_gram@ymail.com>
2019-11-22 15:23:51 +0900

    Added event validation

---
+++ service/shipment/pkg/event_validating/service.go
@@ -0,0 +1,21 @@
+package event_validating
+
+type Service interface {
+	ValidateLatestHistoryEvent(shipmentID string) error
+}
+
+type Repository interface {
+	ValidateLatestHistoryEvent(shipmentID string) error
+}
+
+type service struct {
+	validationRepo Repository
+}
+
+func NewService(validationRepo Repository) Service {
+	return &service{validationRepo}
+}
+
+func (srv *service) ValidateLatestHistoryEvent(shipmentID string) error {
+	return srv.validationRepo.ValidateLatestHistoryEvent(shipmentID)
+}
