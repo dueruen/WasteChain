17ffbcaf5c877ed2dd881ca07798bda5a68ad5c7
Victor Gram Thomsen <victor_gram@ymail.com>
2019-12-13 23:44:37 +0900

    Removed unused code

--- web/src/components/create-account-form.js
+++ web/src/components/create-account-form.js
@@ -39,43 +39,4 @@ const CreateAccountForm = (props) => {
 
 export default CreateAccountForm;
 
-// export default function CreateAccountForm(props) {
-//     return (
-//         <Fragment>
-//             <Wrapper>
-//                 <LoginBox>
-//                     <form action="" method="post" class="form form-login">
-//                         <FormField>
-//                             <label class="user" for="login-username"><span class="hidden">Username</span></label>
-//                             <input id="login-username" type="text" class="form-input" placeholder="Username" required />
-//                         </FormField>
-//                         <FormField>
-//                             <label class="lock" for="login-password"><span class="hidden">Password</span></label>
-//                             <input id="login-password" type="password" class="form-input" placeholder="Password" required />
-//                         </FormField>
-//                         <FormField>
-//                             <input type="submit" value="Log in" />
-//                         </FormField>
-//                     </form>
-//                 </LoginBox>
-//             </Wrapper>
-//         </Fragment>
-//     )
-// }
 
-// const Wrapper = styled.div`
-//     display: flex;
-//     flex-direction: column;
-// `
-
-// const LoginBox = styled.div`
-//     width: '100%';
-//     marginBottom: unit * 5;
-//     padding: unit * 2.5;
-//     position: 'relative';
-// `
-
-// const FormField = styled.div`
-// display: block;
-// margin-bottom: 2rem;
-// `
--- web/src/pages/companies.js
+++ web/src/pages/companies.js
@@ -2,76 +2,8 @@ import React, { Fragment } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
-//import { LaunchTile, Header, Button, Loading } from '../components';
+//TODO
 
-const ls = ({ companies }) => {
-    const { id, name } = companies;
-    return (
-      <div>
-          id: {companies.id}
-      </div>
-    );
-  };
 
-export const LAUNCH_TILE_DATA = gql`
-  fragment ls on Company {
-    id
-    name
-  }
-`;
 
-export const GET_COMPANIES = gql`
-  query listAllCompanies($after: String) {
-    companies(after: $after) {
-      id,
-      name
-    }
-  }
-  ${LAUNCH_TILE_DATA}
-`;
 
-export default function Companies() {
-  const { data, loading, error, fetchMore } = useQuery(GET_COMPANIES);
-  if (loading) return <div>LOADING</div>;
-  if (error) return <p>ERROR</p>, console.log("ERROR: " + error), console.log("DATA: " + data);
-
-  console.log(data);
-  return (
-    <Fragment>
-      <div />
-      <h1>Companies</h1>
-      {/* {data.launches &&
-        data.launches.launches &&
-        data.launches.launches.map(launch => (
-          <LaunchTile key={launch.id} launch={launch} />
-        ))} */}
-      {/* {data.launches &&
-        data.launches.hasMore && (
-          <Button
-            onClick={() =>
-              fetchMore({
-                variables: {
-                  after: data.launches.cursor,
-                },
-                updateQuery: (prev, { fetchMoreResult, ...rest }) => {
-                  if (!fetchMoreResult) return prev;
-                  return {
-                    ...fetchMoreResult,
-                    launches: {
-                      ...fetchMoreResult.launches,
-                      launches: [
-                        ...prev.launches.launches,
-                        ...fetchMoreResult.launches.launches,
-                      ],
-                    },
-                  };
-                },
-              })
-            }
-          >
-            Load More
-          </Button>
-        )} */}
-    </Fragment>
-  );
-}
