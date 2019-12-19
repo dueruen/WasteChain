086230292b2ffe4918d6029dec7fcefa413031c0
dueruen <32197042+dueruen@users.noreply.github.com>
2019-12-19 17:42:23 +0900

    Clean up

--- service/signature/pkg/service.go
+++ service/signature/pkg/service.go
@@ -8,7 +8,6 @@ import (
 
 	pb "github.com/dueruen/WasteChain/service/signature/gen/proto"
 	"github.com/dueruen/WasteChain/service/signature/pkg/event/pub"
-	"github.com/dueruen/WasteChain/service/signature/pkg/event/sub"
 	"github.com/dueruen/WasteChain/service/signature/pkg/key"
 	"github.com/dueruen/WasteChain/service/signature/pkg/sign"
 	"github.com/dueruen/WasteChain/service/signature/pkg/storage/postgres"
@@ -82,13 +81,6 @@ func Run() {
 
 	//Create Sign Service
 	signSrv := sign.NewService(storage, keySrv, pubEventHandler, qrClient, blockClient)
-
-	//Connect Sub to NATS
-	errSub := sub.StartListening(nats, signSrv)
-	if errSub != nil {
-		log.Fatalf("Could not connect to NATS %v", errSub)
-	}
-	fmt.Printf("Sub Connection to NATS service made\n")
 
 	var endpoints transport.Endpoints
 	{
